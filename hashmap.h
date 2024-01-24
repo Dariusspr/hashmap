80bb2dc0322cc0460088baebfe1b3fa8202c5cc5
Dariusspr <darius.spruogis@gmail.com>
2024-01-24 13:32:42 +0200

    removed map_t because 'const map_t' becomes 'void *const' instead of 'const void*'.And added const to some parameters to ensure that passed values are not changed

--- hashmap.h
+++ hashmap.h
@@ -7,14 +7,13 @@
 #define DEFAULT_MAP_LOAD 0.7
 #define DEFAULT_MAP_GROWTH 2.0
 
-typedef void *any_t;
-typedef struct hashmap *map_t;
-typedef size_t (*hash_t)(any_t);
+typedef struct hashmap* map_t;
+typedef size_t (*hash_t)(const void *);
 
 typedef struct
 {
-    any_t (*copy)(const any_t value); // returns a pointer to a copy of value
-    bool (*cmp)(const any_t value1, const any_t value2); // returns true if equal
+    void *(*copy)(const void *value); // returns a pointer to a copy of value
+    bool (*cmp)(const void *value1, const void *value2); // returns true if equal
 } argumentType;
 
 // builtin types
@@ -23,8 +22,8 @@ extern argumentType intType;
 
 // returns map_t object if it is successfully created
 extern map_t hashmap_create(size_t initialCapacity, float loadFactor, float growth, hash_t hashFunction, argumentType keyType, argumentType valueType);
-extern bool hashmap_set(map_t map, any_t key, any_t value); // returns false if failed
-extern const any_t hashmap_get(map_t map, any_t key); // returns NULL if such key doesn't exist
+extern bool hashmap_set(map_t map, const void *key, const void *value); // returns false if failed
+extern const void *hashmap_get(map_t map, const void *key); // returns NULL if such key doesn't exist
 extern void hashmap_free(map_t map);
 
 
